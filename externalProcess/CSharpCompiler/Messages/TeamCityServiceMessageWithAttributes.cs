7d2b8202f54f8f2d312c6e6b9028ea437e457051
simone.busoli <simone.busoli@e3d5ca1c-24b2-eab8-8f21-aab099f6db0f>
2010-09-17 23:24:44 +0000

    Full support for service messages and initial support for Dump

---
+++ externalProcess/CSharpCompiler/Messages/TeamCityServiceMessageWithAttributes.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpCompiler.Messages
+{
+    public abstract class TeamCityServiceMessageWithAttributes : TeamCityServiceMessage
+    {
+        protected TeamCityServiceMessageWithAttributes(string name) : base(name)
+        {
+        }
+
+        protected override string FormattedAndEscapedValue
+        {
+            get
+            {
+                return Properties.Aggregate(new StringBuilder(),
+                                            (builder, pair) =>
+                                            builder.AppendFormat(" {0}='{1}'", pair.Key, Escape(pair.Value)))
+                    .ToString();
+            }
+        }
+
+        protected abstract IEnumerable<KeyValuePair<string, object>> Properties { get; }
+    }
+}
--- externalProcess/CSharpCompiler/ProgramCompiler.cs
+++ externalProcess/CSharpCompiler/ProgramCompiler.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.CodeDom.Compiler;
-using System.Text;
+﻿using System.Text;
 
 namespace CsharpCompiler
 {
