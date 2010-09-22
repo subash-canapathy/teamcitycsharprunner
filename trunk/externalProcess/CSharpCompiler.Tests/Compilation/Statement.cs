ef27b57ef1d2009690c2423fac004abadab5e540
simone.busoli <simone.busoli@e3d5ca1c-24b2-eab8-8f21-aab099f6db0f>
2010-09-22 22:33:34 +0000

    Correctly supporting .NET 2 and 4

---
+++ trunk/externalProcess/CSharpCompiler.Tests/Compilation/Statement.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Runtime.InteropServices;
+using NUnit.Framework;
+
+namespace CsharpCompiler.Tests.Compilation
+{
+	[TestFixture]
+	public class Statement
+	{
+		private CompositeCompiler compiler;
+
+		[SetUp]
+		public void Setup()
+		{
+			compiler = new CompositeCompiler();
+		}
+
+		[Test]
+		public void Simple_statement()
+		{
+			Console.WriteLine(RuntimeEnvironment.GetSystemVersion());
+
+			var result = compiler.Compile("var a = 1;");
+
+			Assert.IsEmpty(result.Errors);
+		}
+
+
+		[Test]
+		[Platform("Net-4.0")]
+		public void Simple_statement_with_dynamic_succeeds_on_NET_v4()
+		{
+			var result = compiler.Compile("dynamic a = 1;");
+
+			Assert.IsEmpty(result.Errors);
+		}
+
+		[Test]
+		[Platform("Net-2.0")]
+		public void Simple_statement_with_dynamic_fails_on_NET_v2()
+		{
+			var result = compiler.Compile("dynamic a = 1;");
+
+			Assert.IsNotEmpty(result.Errors);
+		}
+	}
+}
